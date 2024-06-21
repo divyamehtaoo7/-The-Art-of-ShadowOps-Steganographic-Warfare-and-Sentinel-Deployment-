50127e0222f8db29e336ddef2c559fbb49d6eb9f
divyamehtaoo7 <divyanayankmehtaoo7@gmail.com>
2024-06-22 01:30:23 +0530

    Update pallier.py

--- Week 1: Python & Cryptography Assignment 2/pallier.py
+++ Week 1: Python & Cryptography Assignment 2/pallier.py
@@ -1,5 +1,20 @@
-p=int(input("frist prime number "))
-q=int(input("second prime number  "))
+import random
+def prime(x):
+   t=2
+   while t<x:
+     if x%t==0:
+       return False
+     t=t+1
+     return True
+
+p=2
+q=2
+
+while True :
+  p=random.randint(0,500)
+  q=random.randint(0,500)
+  if(prime(p) and prime(q) and p!=q):
+     break
 n=p*q
 g=n+1
 print("public key is",n,",",n+1)
@@ -7,6 +22,4 @@ w=(p-1)*(q-1)
 t=0
 while (n*t+1)%w !=0:
   t=t+1
-print("private key",n,",",w,",",(n*t+1)/w )  
-
-
+print("private key",n,",",w,",",(n*t+1)/w )
