5e7dee95fec6e284d96968dd3a4a6cd0d01a71c8
divyamehtaoo7 <divyanayankmehtaoo7@gmail.com>
2024-06-22 01:31:09 +0530

    Update rsa.py

--- Week 1: Python & Cryptography Assignment 2/rsa.py
+++ Week 1: Python & Cryptography Assignment 2/rsa.py
@@ -1,21 +1,40 @@
 import random
+def coprime(a,b):
+  w=b
+  while w>0:
+    if a%w==0 and b%w==0 and w!=1:
+      return False
+
+    w=w-1 
+    return True
 def prime(x):
-   t=1
+   t=2
    while t<x:
      if x%t==0:
        return False
      t=t+1
-  return true
-p=0
-q=0
-while !(prime(P) && prime(Q) && p==q) :
-  p=random.randint(0,1000)
-  q=random.randint(0,1000)
+     return True
+
+p=2
+q=2
+
+while True :
+  p=random.randint(10,200)
+  q=random.randint(10,200)
+  if(prime(p) and prime(q) and p!=q):
+     break
 n=p*q
-e=int(input("enter encryptio exponent "))
-print("public key is",n,",",e)
 w=(p-1)*(q-1)
+print(w)
+while True:
+  e=random.randint(50,100)
+  if coprime(w,e):
+    break
+print("public key is",n,",",e)
 t=0
-while (w*t+1)%e!=0:
+while True:
+  if (w*t+1)%e==0:
+    break
   t=t+1
+
 print("private key is ",n,",",(w*t+1)/e)
